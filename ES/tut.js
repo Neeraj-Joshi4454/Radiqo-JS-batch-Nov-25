d77ecc19f4d275bc770606f1cf4b2a5a2e82db2e
Neeraj Joshi <neerajmjoshi1@gmail.com>
2025-12-20 11:17:41 +0530

    ES

---
+++ ES/tut.js
@@ -0,0 +1,126 @@
+// let const var
+
+// var : Global scope variable
+
+// let & const : Local scope variable
+
+// let firstName = "React";
+
+// firstName = "Angular";
+
+// console.log("value of firstName is : ", firstName);
+
+// const PI = 3.14;
+
+// console.log("Value of PI is : ", PI);
+
+// arrow function
+
+// function showName(name){
+//     console.log("Your name is : ", name);
+// }
+
+// syntax :
+
+// const functionName = (para1, para2, ...) => {
+//     statements
+// }
+
+// const showName = (name) => {
+//     console.log("Your name is : ", name)
+// }
+
+// showName("Jhon");
+
+// map
+
+// const users = [
+//     {
+//         id : 1,
+//         name : "jhon Doe",
+//         city : "Pune"
+//     },
+//     {
+//         id : 2,
+//         name : "Rohini",
+//         city : "Pune"
+//     },
+//     {
+//         id : 3,
+//         name : "Geetakshi",
+//         city : "Mumbai"
+//     }
+// ]
+
+
+
+// users.map((user,i) => {
+//     console.log("user is : ", user.name);
+//     console.log("position of user is : ", i);
+// })
+
+
+
+// filter
+
+
+// const users = [
+//     {
+//         id : 1,
+//         name : "jhon Doe",
+//         city : "Pune"
+//     },
+//     {
+//         id : 2,
+//         name : "Rohini",
+//         city : "Pune"
+//     },
+//     {
+//         id : 3,
+//         name : "Geetakshi",
+//         city : "Mumbai"
+//     }
+// ]
+
+
+// const response = users.filter((data , i) => {
+//     return data.city === "Pune";
+// })
+
+// response.map((data) => {
+//     document.getElementById('user').innerHTML+=`<h1>${data.name}</h1>`
+// })
+
+
+// const array1 = ["one", "Two"];
+
+// const array2 = ["Three", "Four"];
+
+
+// const array3 = [...array1,...array2, "Five", "Six"];
+
+// console.log(array3)
+
+
+// const obj1 = {
+//     id : 1,
+//     name : "Jhon"
+// }
+
+// const obj2 = {
+//     city: "Pune"
+// }
+
+
+// const obj3 = {
+//     ...obj1,
+//     ...obj2,
+//     phone: '7972212488'
+// }
+
+// console.log(obj3)
+
+
+
+
+
